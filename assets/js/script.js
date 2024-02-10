39ba897f1e906df07978d975cbd5f988d0a1761b
Ayan <ayanmitra2003@gmail.com>
2024-02-10 20:29:53 +0530

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-const horas = document.getElementById('hours');
-const minutos = document.getElementById('minutes');
-const segundos = document.getElementById('seconds');
-const relogio = document.getElementById('relogio'); /*pego a id da div do relogio */
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const relogio = document.getElementById('relogio'); 
 
 
-
-const clock = setInterval(function time() { /* pegamos uma variavel e atribuimos a data de hoje */
-    let dateToday = new Date();
+const clock = setInterval(function time() 
+{ 
+    let dateToday = new Date(new Date().toLocaleString("en-US", { timeZone: "Europe/Moscow" }));
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
@@ -15,28 +15,29 @@ const clock = setInterval(function time() { /* pegamos uma variavel e atribuimos
     if(min <10) min = '0' + min;
     if(sec <10) sec = '0' + sec;
 
-    horas.textContent = hr;
-    minutos.textContent = min;
-    segundos.textContent = sec;
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
 
-    if(hr >= 6 && hr < 12){
-        //BOM DIA
+    if(hr >= 6 && hr < 12)
+    {
+       
         document.body.style.background = 'linear-gradient(120deg, #FAFFE3, #5DD9CF)';
-        relogio.classList.add('fotomanha'); /* criando uma classe para personalizar no css */
+        relogio.classList.add('fotomanha'); 
         
     } else if(hr >= 12 && hr <18 ){
-        //BOA TARDE
-        document.body.style.background = 'linear-gradient(120deg, #E8A4C5, #7093D8)';
-        relogio.classList.add('fototarde'); /* criando uma classe para personalizar no css */
+        
+        document.body.style.background = 'linear-gradient(180deg, #E8A4C5, #7093D8)';
+        relogio.classList.add('fototarde');
 
     } else if(hr >= 18 && hr < 24 ){
-        //BOA NOITE
-    document.body.style.background = 'linear-gradient(120deg, #906CDD, #2A2553)';
-    relogio.classList.add('fotonoite'); /* criando uma classe para personalizar no css */
+       
+    document.body.style.background = 'linear-gradient(180deg, #906CDD, #2A2553)';
+    relogio.classList.add('fotonoite'); 
 
     } else if(hr < 6 && hr >= 0){
         document.body.style.background = 'linear-gradient(120deg, #185E9B, #3674BE)';
-        relogio.classList.add('fotomadrugada'); /* criando uma classe para personalizar no css */
+        relogio.classList.add('fotomadrugada'); 
     }
 })
 
